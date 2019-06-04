29ffb283cb17340838f701f1c949efa44c683f57
wini <imbatman@wp.pl>
2019-06-04 17:57:56 +0200

    master - fetch i tworzenie pokemona

--- main.js
+++ main.js
@@ -1,15 +1,50 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then(resp => resp.json())
-    .then(resp => {
-        console.log(resp);
+let todoList = null;
+let todoForm = null;
+let todoSearch = null;
 
-        resp.forEach(user => {
-            console.groupCollapsed(`Użytkownik ${user.id}`)
-            console.log(`Nazwa: ${user.name}`);
-            console.log(`Nazwa użytkownika: ${user.username}`);
-            console.log(`Email: ${user.email}`);
-            console.log(`Adres: ${user.address.city} ${user.address.street} ${user.address.zipcode}`);
-            console.log(`WWW: ${user.website}`);
-            console.groupEnd();
+function addTask(text) {
+    console.log('Dodaję zadanie do listy')
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const todoForm = document.querySelector('#pokemonForm');
+    const formData = new FormData();
+
+    const name = null;
+    const type = null;
+    const strength = null;
+    const agility = null;
+
+    try {
+        todoForm.addEventListener('submit', function(e) {
+            console.log("Inside event listener");
+            e.preventDefault();
+            const  name = this.querySelector('input[name="name"]');
+            const  type = this.querySelector('type');
+            const  strength = this.querySelector('strength');
+            const  agility = this.querySelector('agility');
+            console.log("name:",name);
+            console.log("type:",type);
+
+            sendCreateData(name,type,strength,agility);
+        });
+
+        sendCreateData(formData);
+    } catch (error) {
+        console.log("Blad", error);
+    }
+   
+});
+
+function sendCreateData(name,type, strength, agility) {
+    let pokemonObject;
+    console.log("Sending submit");
+    fetch("http://localhost:8000/api/pokemons/create?name=" + name.toString() + "&type=" + type.toString() + "&strength=" + strength.toString() + "&agility=" + agility.toString(), {
+           method:"get",
+            mode:"no-cors"
         })
-    })
+        .then(res => res.json())
+        .then(data => pokemonObject = data)
+
+    return pokemonObject;
+}
